5998616f71eaa0e61bf30a23abf9dac5057f0844
cq10086123 <105488660+cq10086123@users.noreply.github.com>
2025-05-21 09:03:38 +0800

    db.config.php

--- include/db.config.php
+++ include/db.config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_HOST','mysql11.serv00.com');//数据库连接地址，默认：localhost或127.0.0.1
-define('DB_USER','m8179_yz');//数据库账号
-define('DB_PASSWD','SVIm9Tn3rg6y%oyhVu&gNwHJD5TZ;3');//数据库密码
-define('DB_NAME','m8179_yz');//数据库名称
+define('DB_HOST','');//数据库连接地址，默认：localhost或127.0.0.1
+define('DB_USER','');//数据库账号
+define('DB_PASSWD','');//数据库密码
+define('DB_NAME','');//数据库名称
 define('DB_PRE','eruyi_');//数据库前缀
 ?>
